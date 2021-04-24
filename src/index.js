59c00ce11f2e198ce2b077579aa02973e817369e
Jose Hernandez <josemanuelhdez2@gmail.com>
2021-04-24 22:45:17 +0100

    WASD movement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Phaser from "phaser";
 import imgAtlas from "./assets/ld48-a.png";
 import dudeImg from "./assets/dude.png";
-import musicFile from "./assets/audio/sc-money.mp3";
+import musicFile from "./assets/audio/shipping-lanes.ogg";
 import { createSpeechBubble } from "./util/bubble";
 import { createStrokeText } from "./util/text";
 import atlasJson from "./assets/atlas.json";
@@ -14,7 +14,14 @@ const config = {
   pixelArt: true,
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+        debug: true
+    }
   },
   audio: {
     disableWebAudio: true
@@ -30,6 +37,8 @@ function preload() {
   this.load.atlas('atlas', imgAtlas, atlasJson);
 }
 
+var cursors, player;
+
 function create() {
   const logo = this.add.image(400, 350, "imgAtlas");
   const dude = this.add.image(100, 550, "dude") //.setScale(2);
@@ -38,11 +47,11 @@ function create() {
 
   // createSpeechBubble(this, 70, 400, 250, 100, "“And now you're a boss, too... of this pile of rubble.”" );
 
-  // createStrokeText(this, 100, 100, "Hello gorgeous", {
-  //   fontFamily: "Arial Black",
-  //   fontSize: 74,
-  //   color: "#c51b7d"
-  // });
+  createStrokeText({thiz: this, x: 100, y: 20, text: "DEEPER BLUE", style: {
+    fontFamily: "Arial Black",
+    fontSize: 74,
+    color: "white"
+  }, stroke: {color: "blue", size: 6}});
 
   this.tweens.add({
     targets: logo,
@@ -52,6 +61,16 @@ function create() {
     yoyo: true,
     loop: -1
   });
+
+  cursors = this.input.keyboard.createCursorKeys();
+  cursors = this.input.keyboard.addKeys(
+    {up:Phaser.Input.Keyboard.KeyCodes.W,
+    down:Phaser.Input.Keyboard.KeyCodes.S,
+    left:Phaser.Input.Keyboard.KeyCodes.A,
+    right:Phaser.Input.Keyboard.KeyCodes.D});
+  
+  player = this.physics.add.image(400, 300, 'dude');
+  player.setCollideWorldBounds(true);
 
   // tgame.add.button(
   //   game.world.centerX - 95,
@@ -67,4 +86,23 @@ function create() {
   // var music = this.sound.add("theme");
 
   // music.play();
+}
+
+const speed = 300;
+
+function update() {
+  // console.log("hello")
+  player.setVelocity(0);
+
+  if (cursors.left.isDown) {
+      player.setVelocityX(-speed);
+  } else if (cursors.right.isDown) {
+      player.setVelocityX(300);
+  }
+
+  if (cursors.up.isDown) {
+      player.setVelocityY(-speed);
+  } else if (cursors.down.isDown) {
+      player.setVelocityY(speed);
+  }
 }
