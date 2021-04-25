2cb7d0daa2998b4c2bc7fd58b3ef7fd923c72549
Jose Hernandez <josemanuelhdez2@gmail.com>
2021-04-25 17:55:50 +0100

    new gfx, fix particles code

--- src/utils/particles.ts
+++ src/utils/particles.ts
@@ -6,17 +6,14 @@ export const playerParticles = ({scene, ship, texture, frame}:
     var emitter = particles.createEmitter({
         frame: frame,
         speed: () => {
-                return 1 /* ship.body.speed */;
+            return 1 /* ship.body.speed */;
         },
-        lifespan: () =>
-            {
-                return Phaser.Math.Percent(/* ship.body.speed */ 1, 0, 300) * 20000;
-            }
-        ,
+        lifespan: () => {
+            return Phaser.Math.Percent(/* ship.body.speed */ 1, 0, 300) * 20000;
+        },
         alpha: () => {
-                return Phaser.Math.Percent(/* ship.body.speed */ 1, 0, 300) * 1000;
-            }
-        ,
+            return Phaser.Math.Percent(/* ship.body.speed */ 1, 0, 300) * 1000;
+        },
         scale: { start: 1.0, end: 0 },
         blendMode: 'ADD'
     });
